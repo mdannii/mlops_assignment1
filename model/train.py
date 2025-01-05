8125457773e38637e3a0b988db10831680fa7743
danny maisnam <mdannii@gmail.com>
2025-01-05 20:20:46 +0530

    modified train.py

--- model/train.py
+++ model/train.py
@@ -10,7 +10,9 @@ data = pd.read_csv('data/dataset.csv')
 X = data[['feature1', 'feature2']]
 y = data['target']
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
 
 # Train model
 model = LinearRegression()
